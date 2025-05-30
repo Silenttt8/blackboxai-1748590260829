df51b7fa2b6501ec165b92e4e04259b6858148ed
Silenttt. <dandyrr5@gmail.com>
2025-05-30 14:31:50 +0700

    Add AWKSC/kontak.php 1748590305682

---
+++ AWKSC/kontak.php
@@ -0,0 +1,22 @@
+<?php 
+	include 'header.php';
+	include 'koneksi.php';	
+?>
+
+<div class="container">
+	<div class="panel">
+		<div class="panel-heading">
+			<h4>Kontak</h4>
+		</div>
+		<div class="panel-body">
+			<p>		
+				<b>Nomor Telepon : </b> 021-7987654 <br/>
+				<b>Nomor HP / WA &nbsp;: </b> 098765432123 <br/>
+				<b>Email : </b> www.AWKSC@gmail.com <br/>
+				<b>Lokasi : </b> JL. GENDING RAYA BLOK F 52 Bekasi, Jawa Barat <br/>
+			</p>       
+		</div>
+	</div>
+</div>
+
+<?php include 'footer.php'; ?>
