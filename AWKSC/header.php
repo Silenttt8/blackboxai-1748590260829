4929a8b8c3e7931a997e9c9145e63e384f5ee0ab
Silenttt. <dandyrr5@gmail.com>
2025-05-30 14:31:58 +0700

    Add AWKSC/header.php 1748590314270

---
+++ AWKSC/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>AWK SPORT CENTER</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+		<script type="text/javascript" src="assets/js/jquery.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body style="background: #f0f0f0">
+	<!-- menu navigasi -->
+	<nav class="navbar navbar-inverse" style="border-radius: 0px">
+		<div class="container-fluid">			
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php"><i class="glyphicon glyphicon-home"></i> Beranda</a></li>
+					<li><a href="produk.php"><i class="glyphicon glyphicon-list-alt"></i> Produk</a></li>
+					<li><a href="kontak.php"><i class="glyphicon glyphicon-envelope"></i> Kontak</a></li>
+					<li><a href="cart.php"><i class="glyphicon glyphicon-shopping-cart"></i> Keranjang</a></li>
+				</ul>				
+				<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="glyphicon glyphicon-log"></i> LOGIN <span class="caret"></span></a>
+						<ul class="dropdown-menu">							
+							<li><a href="index_admin.php"><i class="glyphicon glyphicon-lock"></i> Admin</a></li>
+							<li><a href="form_login.php"><i class="glyphicon glyphicon-lock"></i> Pelanggan</a></li>
+						</ul>
+					</li>
+				
+			</div>
+		</div>
+	</nav>
+	<!-- akhir menu navigasi -->
